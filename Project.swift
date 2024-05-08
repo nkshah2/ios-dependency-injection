04c272a7f2d763b97b6c3630fca0b7c5d2e623ac
Nemi Shah <nemishah1212@gmail.com>
2024-05-08 16:47:48 +0530

    Edit project

--- Project.swift
+++ Project.swift
@@ -8,24 +8,8 @@ let project = Project(
             destinations: .iOS,
             product: .framework,
             bundleId: "com.tbx.IosDependencyInjection",
-            infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
-                ]
-            ),
             sources: ["IosDependencyInjection/Sources/**"],
-            resources: ["IosDependencyInjection/Resources/**"],
             dependencies: []
-        ),
-        .target(
-            name: "IosDependencyInjectionTests",
-            destinations: .iOS,
-            product: .unitTests,
-            bundleId: "com.tbx.IosDependencyInjectionTests",
-            infoPlist: .default,
-            sources: ["IosDependencyInjection/Tests/**"],
-            resources: [],
-            dependencies: [.target(name: "IosDependencyInjection")]
-        ),
+        )
     ]
 )
